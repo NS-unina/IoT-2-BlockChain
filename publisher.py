f1da87ce3e0c5ec5a14482d5997d087fadfd88b3
NdA994 <nda994@gmail.com>
2022-06-09 06:09:10 -0700

    fix publisher

--- publisher.py
+++ publisher.py
@@ -6,21 +6,6 @@ import time
 from paho.mqtt import client as mqtt_client
 from mqttnode import publisher
 
-def publish(client):
-    msg_count = 0
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
-
-
 def run():
     connector = publisher()
     connector.connect_mqtt()
