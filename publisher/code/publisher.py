023b49faec40418e89d855f26735b74880f861c6
NdA994 <nda994@gmail.com>
2022-06-10 03:48:12 -0700

    add publisher

---
+++ publisher/code/publisher.py
@@ -0,0 +1,18 @@
+# python 3.6
+
+import random
+import time
+
+from paho.mqtt import client as mqtt_client
+from mqttnode import publisher
+
+def main():
+    connector = publisher()
+    connector.connect_mqtt()
+    connector.client.loop_start()
+    while True:
+        time.sleep(5)
+        connector.publish(random.randint(0,10))
+
+if __name__ == '__main__':
+    main()
